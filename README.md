39f2d3c21174b030ceebdcbb2fe575dae5eb241e
DEO <f.db@163.com>
2016-10-14 10:37:54 +0800

    初始化项目

---
+++ README.md
@@ -0,0 +1,3 @@
+The IBM® Speech to Text service provides an API that enables you to add IBM's speech recognition capabilities to your applications. The service transcribes speech from various languages and audio formats to text with low latency. For most languages, the service supports two sampling rates, broadband and narrowband.
+
+> npm install watson-developer-cloud
---
+++ index.ts
@@ -0,0 +1,25 @@
+var SpeechToTextV1 = require('watson-developer-cloud/speech-to-text/v1');
+var fs = require('fs');
+
+var speech_to_text = new SpeechToTextV1({
+  username: '<username>',
+  password: '<password>'
+});
+
+var params = {
+  // From file
+  audio: fs.createReadStream('./resources/speech.wav'),
+  content_type: 'audio/l16; rate=44100'
+};
+
+speech_to_text.recognize(params, function(err, res) {
+  if (err)
+    console.log(err);
+  else
+    console.log(JSON.stringify(res, null, 2));
+});
+
+// or streaming
+fs.createReadStream('./resources/speech.wav')
+  .pipe(speech_to_text.createRecognizeStream({ content_type: 'audio/l16; rate=44100' }))
+  .pipe(fs.createWriteStream('./transcription.txt'));
